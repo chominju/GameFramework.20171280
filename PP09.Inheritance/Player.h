efb7f6fafa2b3ab883b7b69f43d7177eacbfca74
min ju cho <20171280@game.hoseo.edu>
2018-10-29 11:42:15 +0900

    10월 29일 PP09.Inheritance

--- PP09.Inheritance/Player.h
+++
@@ -1,9 +0,0 @@
-class Player : public GameObject
-{
-public:
-    void load(int x, int y, int width, int height,
-        std::string textureID);
-    void draw(SDL_Renderer* pRenderer);
-    void update();
-    void clean();
-};
